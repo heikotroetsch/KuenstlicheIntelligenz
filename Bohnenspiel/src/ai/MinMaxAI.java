261d155fc5aa3a38a53356de8be1935be077908c
salpers <sebialpers@gmail.com>
2018-10-05 17:30:52 +0200

    Alpha Beta Test

--- Bohnenspiel/src/ai/MinMaxAI.java
+++ Bohnenspiel/src/ai/MinMaxAI.java
@@ -10,6 +10,9 @@ public class MinMaxAI extends AI {
 	boolean initialized = false;
 	Random rand = new Random();
 
+	static int MAX = 1000;
+	static int MIN = -1000;
+
 	public MinMaxAI() {
 		state = new BohnenspielState();
 	}
@@ -23,47 +26,55 @@ public class MinMaxAI extends AI {
 			initialized = true;
 			state = new BohnenspielState(state, index);
 		}
-		// enemy acted and i have to react	---   1-6
+		// enemy acted and i have to react --- 1-6
 		else if (enemyIndex > 0 && enemyIndex <= 6) {
 			state = new BohnenspielState(state, --enemyIndex);
 			if (!initialized) {
 				state.setAiTurn(true, false);
-				initialized = true;				
+				initialized = true;
 			}
 			index = findBestMove(state);
 			state = new BohnenspielState(state, index);
-		//enemy acted and i have to react   ---   7-12	
+			// enemy acted and i have to react --- 7-12
 		} else if (enemyIndex > 6 && enemyIndex <= 12) {
 			state = new BohnenspielState(state, --enemyIndex);
 			if (!initialized) {
 				state.setAiTurn(true, true);
-				initialized = true;				
+				initialized = true;
 			}
 			index = findBestMove(state);
 			state = new BohnenspielState(state, index);
 		}
 		System.out.println(state);
-		return index+1;
+		return index + 1;
 	}
 
-	private int minimax(BohnenspielState bss, int depth, boolean isMaximizingPlayer) {
+	private int minimax(BohnenspielState bss, int depth, int alpha, int beta, boolean isMaximizingPlayer) {
 		if (depth == 0) {
 			return bss.calculateHeuristivValue();
 		}
 		if (isMaximizingPlayer) {
-			int best = -1000;
+			int value = MIN;
 			// Traverse all possible moves
 			for (BohnenspielState bs : bss.expand()) {
-				best = Math.max(best, minimax(bs, depth - 1, !isMaximizingPlayer));
+				value = Math.max(value, minimax(bs, depth - 1, alpha, beta, !isMaximizingPlayer));
+				alpha = Math.max(alpha, value);
+				if (alpha >= beta) {
+					break;
+				}
 			}
-			return best;
+			return value;
 		} else {
-			int best = 1000;
+			int value = MAX;
 			// Traverse all possible moves
 			for (BohnenspielState bs : bss.expand()) {
-				best = Math.min(best, minimax(bs, depth - 1, !isMaximizingPlayer));
+				value = Math.min(value, minimax(bs, depth - 1, alpha, beta, !isMaximizingPlayer));
+				beta = Math.min(beta, value);
+				if (alpha >= beta) {
+					break;
+				}
 			}
-			return best;
+			return value;
 		}
 	}
 
@@ -73,17 +84,16 @@ public class MinMaxAI extends AI {
 
 		// Traverse all possible moves
 		for (BohnenspielState bs : bss.expand()) {
-			int moveVal = minimax(bss, 10, true);
+			int moveVal = minimax(bss, 10, MIN, MAX, true);
 //			System.out.println("moveVal: "+moveVal);
 			if (moveVal > bestVal) {
 				bestMove = bs.getLastMove();
 				bestVal = moveVal;
 			}
 		}
-	System.out.println("Value of the best Move is: " + bestVal);
+		System.out.println("Value of the best Move is: " + bestVal);
 		return bestMove;
 	}
-
 
 	@Override
 	public String getName() {
